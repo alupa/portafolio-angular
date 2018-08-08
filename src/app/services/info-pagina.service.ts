d590d8174566fa3f0c02bbfe1f123617f87aab0b
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-08-08 12:27:54 -0400

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,15 +10,30 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
     //console.log('Servicio de infoPagina cargada');
     //Leer el archivo Json
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+          //console.log(resp);
           //console.log( resp['twitter'] );
         });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://html-angular-aa5b2.firebaseio.com/equipo.json')
+        .subscribe( (resp: any) => {
+          this.equipo = resp;
+          console.log(resp);
+        }); 
   }
 }
